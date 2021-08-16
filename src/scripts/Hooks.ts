69e223357483f93264dda62c1f30acb719e4e640
Marco Tenti <marco.tenti@abd.it>
2021-08-16 15:21:42 +0200

    [BETA] Third Commit

---
+++ src/scripts/Hooks.ts
@@ -0,0 +1,75 @@
+import {
+    patch_CombatTracker_getEntryContextOptions,
+    patch_CombatTracker_activateListeners,
+} from "./scripts/patches.js";
+import { handlePreUpdateCombat, handleUpdateCombat } from "./scripts/handleUpdateCombat.js";
+import CONST from "./scripts/const.js";
+import CombatAlertsApplication from "./apps/CombatAlertsApplication.js";
+import TurnAlert from "./scripts/TurnAlert.js";
+import TurnAlertConfig from "./apps/TurnAlertConfig.js";
+
+export let readyHooks = async () => {
+
+  Hooks.on("preUpdateCombat", handlePreUpdateCombat);
+  Hooks.on("updateCombat", handleUpdateCombat);
+
+  Hooks.on("renderCombatTracker", (tracker, html, data) => {
+      if (!data.combat?.data?.round) return;
+
+      const alertButton = $(document.createElement("a"));
+      alertButton.addClass(["combat-control", "combat-alerts"]);
+      alertButton.attr("title", game.i18n.localize(`${CONST.moduleName}.APP.CombatAlertsTitle`));
+      alertButton.html('<i class="fas fa-bell"></i>');
+      alertButton.click((event) => {
+          const combatId = data.combat.id;
+          const app = new CombatAlertsApplication({ combatId });
+          app.render(true);
+      });
+
+      html.find("header#combat-round h3").after(alertButton);
+  });
+
+}
+
+export const setupHooks = async () => {
+
+  // setup all the hooks
+
+
+}
+
+export const initHooks = () => {
+  warn("Init Hooks processing");
+
+  globalThis.TurnAlert = TurnAlert;
+    globalThis.TurnAlertConfig = TurnAlertConfig;
+
+    patch_CombatTracker_activateListeners();
+    patch_CombatTracker_getEntryContextOptions();
+
+    game.socket.on(`module.${CONST.moduleName}`, async (payload) => {
+        const firstGm = game.users.find((u) => u.isGM && u.active);
+        switch (payload.type) {
+            case "createAlert":
+                if (!firstGm || game.user !== firstGm) break;
+                await TurnAlert.create(payload.alertData);
+                break;
+            case "updateAlert":
+                if (!firstGm || game.user !== firstGm) break;
+                await TurnAlert.update(payload.alertData);
+                break;
+            case "deleteAlert":
+                if (!firstGm || game.user !== firstGm) break;
+                await TurnAlert.delete(payload.combatId, payload.alertId);
+                break;
+            default:
+                throw new Error(
+                    `Turn Alert | Unknown socket payload type: ${payload.type} | payload contents:\n${JSON.stringify(
+                        payload
+                    )}`
+                );
+                break;
+        }
+    });
+
+}
--- src/turn-alert.ts
+++ src/turn-alert.ts
@@ -1,61 +1,92 @@
-import {
-    patch_CombatTracker_getEntryContextOptions,
-    patch_CombatTracker_activateListeners,
-} from "./scripts/patches.js";
-import { handlePreUpdateCombat, handleUpdateCombat } from "./scripts/handleUpdateCombat.js";
-import CONST from "./scripts/const.js";
-import CombatAlertsApplication from "./apps/CombatAlertsApplication.js";
-import TurnAlert from "./scripts/TurnAlert.js";
-import TurnAlertConfig from "./apps/TurnAlertConfig.js";
+/**
+ * This is your TypeScript entry file for Foundry VTT.
+ * Register custom settings, sheets, and constants using the Foundry API.
+ * Change this heading to be more descriptive to your module, or remove it.
+ * Author: [your name]
+ * Content License: [copyright and-or license] If using an existing system
+ * 					you may want to put a (link to a) license or copyright
+ * 					notice here (e.g. the OGL).
+ * Software License: [your license] Put your desired license here, which
+ * 					 determines how others may use and modify your module
+ */
+// Import JavaScript modules
 
-Hooks.on("init", () => {
-    globalThis.TurnAlert = TurnAlert;
-    globalThis.TurnAlertConfig = TurnAlertConfig;
+// Import TypeScript modules
+import { getGame, registerSettings } from './module/settings';
+import { preloadTemplates } from './module/preloadTemplates';
+import { TURN_ALERT_MODULE_NAME } from './module/settings';
+import { initHooks, readyHooks, setupHooks } from './module/Hooks';
+// import { installedModules, setupModules } from './module/setupModules';
 
-    patch_CombatTracker_activateListeners();
-    patch_CombatTracker_getEntryContextOptions();
+export let debugEnabled = 0;
+// 0 = none, warnings = 1, debug = 2, all = 3
+export let debug = (...args) => {if (debugEnabled > 1) console.log(`DEBUG:${TURN_ALERT_MODULE_NAME} | `, ...args)};
+export let log = (...args) => console.log(`${TURN_ALERT_MODULE_NAME} | `, ...args);
+export let warn = (...args) => {if (debugEnabled > 0) console.warn(`${TURN_ALERT_MODULE_NAME} | `, ...args)};
+export let error = (...args) => console.error(`${TURN_ALERT_MODULE_NAME} | `, ...args);
+export let timelog = (...args) => warn(`${TURN_ALERT_MODULE_NAME} | `, Date.now(), ...args);
 
-    game.socket.on(`module.${CONST.moduleName}`, async (payload) => {
-        const firstGm = game.users.find((u) => u.isGM && u.active);
-        switch (payload.type) {
-            case "createAlert":
-                if (!firstGm || game.user !== firstGm) break;
-                await TurnAlert.create(payload.alertData);
-                break;
-            case "updateAlert":
-                if (!firstGm || game.user !== firstGm) break;
-                await TurnAlert.update(payload.alertData);
-                break;
-            case "deleteAlert":
-                if (!firstGm || game.user !== firstGm) break;
-                await TurnAlert.delete(payload.combatId, payload.alertId);
-                break;
-            default:
-                throw new Error(
-                    `Turn Alert | Unknown socket payload type: ${payload.type} | payload contents:\n${JSON.stringify(
-                        payload
-                    )}`
-                );
-                break;
-        }
-    });
+export let i18n = key => {
+  return getGame().i18n.localize(key);
+};
+export let i18nFormat = (key, data = {}) => {
+  return getGame().i18n.format(key, data);
+}
+
+export let setDebugLevel = (debugText: string) => {
+  debugEnabled = {"none": 0, "warn": 1, "debug": 2, "all": 3}[debugText] || 0;
+  // 0 = none, warnings = 1, debug = 2, all = 3
+  if (debugEnabled >= 3) CONFIG.debug.hooks = true;
+}
+
+/* ------------------------------------ */
+/* Initialize module					*/
+/* ------------------------------------ */
+Hooks.once('init', async () => {
+	console.log(`${TURN_ALERT_MODULE_NAME} | Initializing ${TURN_ALERT_MODULE_NAME}`);
+
+	// Register custom module settings
+	registerSettings();
+
+	initHooks();
+	// Assign custom classes and constants here
+
+	// Register custom module settings
+	//registerSettings();
+	//fetchParams();
+
+	// Preload Handlebars templates
+	await preloadTemplates();
+	// Register custom sheets (if any)
+
 });
 
-Hooks.on("preUpdateCombat", handlePreUpdateCombat);
-Hooks.on("updateCombat", handleUpdateCombat);
+/* ------------------------------------ */
+/* Setup module							*/
+/* ------------------------------------ */
+Hooks.once('setup', function () {
+	// Do anything after initialization but before ready
+	//setupModules();
 
-Hooks.on("renderCombatTracker", (tracker, html, data) => {
-    if (!data.combat?.data?.round) return;
+  setupHooks();
 
-    const alertButton = $(document.createElement("a"));
-    alertButton.addClass(["combat-control", "combat-alerts"]);
-    alertButton.attr("title", game.i18n.localize(`${CONST.moduleName}.APP.CombatAlertsTitle`));
-    alertButton.html('<i class="fas fa-bell"></i>');
-    alertButton.click((event) => {
-        const combatId = data.combat.id;
-        const app = new CombatAlertsApplication({ combatId });
-        app.render(true);
-    });
+	registerSettings();
 
-    html.find("header#combat-round h3").after(alertButton);
 });
+
+/* ------------------------------------ */
+/* When ready							*/
+/* ------------------------------------ */
+Hooks.once('ready', () => {
+
+	// Do anything once the module is ready
+	// if (!getGame().modules.get("lib-wrapper")?.active && getGame().user.isGM){
+   	// 	ui.notifications.error(`The '${TURN_ALERT_MODULE_NAME}' module requires to install and activate the 'libWrapper' module.`);
+	// 	return;
+	// }
+
+	readyHooks();
+});
+
+// Add any additional hooks if necessary
+
